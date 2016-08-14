bc5f65fc7a4e218e28b27588511782c909b27f1a
Jimmy Lin <usmcjimmy@gmail.com>
2016-08-14 10:09:12 -0400

    unable to add vehicle condition based on vin number

--- app/angular-app/vehicle/condition/addcondition.controller.js
+++ app/angular-app/vehicle/condition/addcondition.controller.js
@@ -11,9 +11,10 @@ function AddConditionController($scope, addConditionProvider){
     'submitter': "submitter",
     'owner': '617a69c3-bd34-445c-a762-1bbcf228f7bb',
     'condition': vm.condition,
+    'vin': vm.vin,
     'activity': 'add'
   })
-
+    console.log($scope.vin)
     alert('clicked')
     addConditionProvider.addConditionData(conditionData)
 
