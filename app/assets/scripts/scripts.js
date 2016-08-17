355771b5d96be4ac4d0c8850eb699c7b90f5d04a
emineKoc <eminekoc@gmail.com>
2016-08-17 15:42:23 -0400

    toggle withween the modals

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -15,3 +15,24 @@ naBaseApp.directive("toggleModal", function() {
     }
   }
 });
+
+naBaseApp.directive("changeModal", function() {
+  return {
+    link: function($scope, element, attr) {
+      element.on("click", function() {
+          $('.signin').css('display','none');
+          $('.signup').css('display','block')
+      });
+    }
+  }
+});
+naBaseApp.directive("signIn", function() {
+  return {
+    link: function($scope, element, attr) {
+      element.on("click", function() {
+          $('.signup').css('display','none');
+          $('.signin').css('display','block')
+      });
+    }
+  }
+});
