9668dc8072aef279be4d5dbc326dd92ebbc0f91f
Brian Fling <fling@me.com>
2016-08-17 21:40:17 -0700

    add active tab control

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -1,9 +1,34 @@
 (function() {
   'use strict';
+  
   $(document).ready(function() {
 
   });
 }());
+
+angular.module('naBaseApp')
+.directive('activeLink', ['$location', function ($location) {
+return {
+    restrict: 'A', //use as attribute 
+    replace: false,
+    link: function (scope, elem) {
+        //after the route has changed
+        scope.$on("$routeChangeSuccess", function () {
+            var hrefs = ['/#' + $location.path(),
+                         '#' + $location.path(), //html5: false
+                         $location.path()]; //html5: true
+            angular.forEach(elem.find('a'), function (a) {
+                a = angular.element(a);
+                if (-1 !== hrefs.indexOf(a.attr('href'))) {
+                    a.parent().addClass('active');
+                } else {
+                    a.parent().removeClass('active');   
+                };
+            });     
+        });
+    }
+}
+}]);
 
 
 naBaseApp.directive("toggleModal", function() {
@@ -12,6 +37,19 @@ naBaseApp.directive("toggleModal", function() {
       element.on("click", function() {
           $('body').toggleClass("modal");
       });
+    }
+  }
+});
+
+naBaseApp.directive("activeNav", function() {
+  return {
+    link: function($scope, element, attr) {
+      element.on("click", function() {
+      
+//        $('header.main nav a').removeClass("active");
+//        $(this).addClass("active");
+      });
+      
     }
   }
 });
