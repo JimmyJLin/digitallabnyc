742736a138896881d622ffcde26b89e742e4b992
emineKoc <eminekoc@gmail.com>
2016-08-18 14:46:34 -0400

    Vehicle details

--- app/angular-app/vehicle/vehicle_details/vehicle_details.controller.js
+++ app/angular-app/vehicle/vehicle_details/vehicle_details.controller.js
@@ -1,11 +1,10 @@
 angular.module('naBaseApp').controller('VehicleDetailsController', VehicleDetailsController);
 
 
-function VehicleDetailsController($routeParams, vehicleDetailsProvider){
+function VehicleDetailsController($routeParams, $scope, vehicleDetailsProvider){
   var vm = this;
   var vehicleID = $routeParams.vehicleID;
 
-  console.log('this is the vehicle guid', vehicleID)
   // $scope.vehicle = vehicleDetailsProvider.vehicleDisplay(vehicleID, function(err, vehicle){
   //     $scope.finished_loading = true;
   //     if (err) {
@@ -16,4 +15,22 @@ function VehicleDetailsController($routeParams, vehicleDetailsProvider){
   //     }
   // })
 
+$scope.vehicle = {
+  "ID":1,
+  "ItemGuid":"617a69c3-bd34-445c-a762-1bbcf228f7bb",
+  "ItemName":"",
+  "DateCreated":"2016-08-08T21:20:34.817",
+  "DateLastModified":"2016-08-09T07:55:04.11",
+  "IsActive":true,
+  "IsDeleted":false,
+  "Notes":"",
+  "VIN":"1GNEK13Z34J221437",
+  "Year":"2013",
+  "Make":"ford",
+  "Model":"fusion",
+  "Type":"25000",
+  "Description":"Black, with faded sports stripes. Here are edited notes.",
+  "Price":"25000",
+  "OwnerGuid":"617a69c3-bd34-445c-a762-1bbcf228f7bb"
+  }
 }
