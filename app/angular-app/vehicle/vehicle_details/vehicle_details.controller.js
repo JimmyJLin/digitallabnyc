b573d10d88f7041c45ab5ae9139edae1e60c47fc
Jimmy Lin <usmcjimmy@gmail.com>
2016-08-19 13:22:25 -0400

    completed vehicle detail

---
+++ app/angular-app/vehicle/vehicle_details/vehicle_details.controller.js
@@ -0,0 +1,39 @@
+angular.module('naBaseApp').controller('VehicleDetailsController', VehicleDetailsController);
+
+
+function VehicleDetailsController($routeParams, $scope, vehicleDetailsProvider){
+  var vm = this;
+  var vehicleID = $routeParams.vehicleID;
+  console.log('this is the vehicle ID', vehicleID)
+
+  $scope.vehicle = vehicleDetailsProvider.vehicleDisplay( vehicleID, function(err, vehicle){
+      $scope.finished_loading = true;
+      if (err) {
+          $scope.page_load_error = err.message;
+      } else {
+           $scope.vehicle = vehicle;
+           console.log("controller Vehicle " + vehicle);
+           vm.vehicle = JSON.parse(vehicle)
+      }
+  })
+}
+
+// $scope.vehicle = {
+//   "ID":1,
+//   "ItemGuid":"617a69c3-bd34-445c-a762-1bbcf228f7bb",
+//   "ItemName":"",
+//   "DateCreated":"2016-08-08T21:20:34.817",
+//   "DateLastModified":"2016-08-09T07:55:04.11",
+//   "IsActive":true,
+//   "IsDeleted":false,
+//   "Notes":"",
+//   "VIN":"1GNEK13Z34J221437",
+//   "Year":"2013",
+//   "Make":"ford",
+//   "Model":"fusion",
+//   "Type":"25000",
+//   "Description":"Black, with faded sports stripes. Here are edited notes.",
+//   "Price":"25000",
+//   "OwnerGuid":"617a69c3-bd34-445c-a762-1bbcf228f7bb"
+//   }
+// }
