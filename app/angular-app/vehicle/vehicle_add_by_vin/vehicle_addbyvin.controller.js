9550f8c80536c896d21d5a4bb2058af48b85c44e
Jimmy Lin <usmcjimmy@gmail.com>
2016-08-21 17:08:09 -0400

    fixed minor api call errors, able to post data to api

--- app/angular-app/vehicle/vehicle_add_by_vin/vehicle_addbyvin.controller.js
+++ app/angular-app/vehicle/vehicle_add_by_vin/vehicle_addbyvin.controller.js
@@ -178,17 +178,18 @@ function VehicleAddbyVinController($scope, addbyVinProvider){
             vm.itemNo = $scope.vehicle.ItemGuid;
             vm.addedsDate = $scope.vehicle.DateCreated;
             // vm.engine = JSON.parse($scope.vehicle.DataSquishDetail).engine;
-            vm.availability = JSON.parse($scope.vehicle.DataSquishDetail).engine.availability;
+            // vm.availability = JSON.parse($scope.vehicle.DataSquishDetail).engine.availability;
             vm.cylinder = JSON.parse($scope.vehicle.DataSquishDetail).engine.cylinder;
-            vm.compressionRatio = JSON.parse($scope.vehicle.DataSquishDetail).engine.compressionRatio;
+            // vm.compressionRatio = JSON.parse($scope.vehicle.DataSquishDetail).engine.compressionRatio;
             vm.size = JSON.parse($scope.vehicle.DataSquishDetail).engine.size;
-            vm.displacement = JSON.parse($scope.vehicle.DataSquishDetail).engine.displacement;
+            vm.configuration = JSON.parse($scope.vehicle.DataSquishDetail).engine.configuration;
+            // vm.displacement = JSON.parse($scope.vehicle.DataSquishDetail).engine.displacement;
             vm.fuelType = JSON.parse($scope.vehicle.DataSquishDetail).engine.fuelType;
-            vm.horsepower = JSON.parse($scope.vehicle.DataSquishDetail).engine.horsepower;
-            vm.torque = JSON.parse($scope.vehicle.DataSquishDetail).engine.torque;
-            vm.totalValves = JSON.parse($scope.vehicle.DataSquishDetail).engine.totalValves;
-            vm.manufacturerEgineCode = JSON.parse($scope.vehicle.DataSquishDetail).engine.manufacturerEgineCode;
-            vm.type = JSON.parse($scope.vehicle.DataSquishDetail).engine.type;
+            // vm.horsepower = JSON.parse($scope.vehicle.DataSquishDetail).engine.horsepower;
+            // vm.torque = JSON.parse($scope.vehicle.DataSquishDetail).engine.torque;
+            // vm.totalValves = JSON.parse($scope.vehicle.DataSquishDetail).engine.totalValves;
+            // vm.manufacturerEgineCode = JSON.parse($scope.vehicle.DataSquishDetail).engine.manufacturerEgineCode;
+            // vm.type = JSON.parse($scope.vehicle.DataSquishDetail).engine.type;
             console.log( vm.vin , $scope.vehicle);
 
         }
@@ -221,19 +222,13 @@ function VehicleAddbyVinController($scope, addbyVinProvider){
         'seatCovering':vm.seatCovering,
         'doorCount':vm.doorCount,
         'engine':{
-          'availability': vm.availability,
           'cylinder': vm.cylinder,
-          'compressionRatio': vm.compressionRatio,
           'size': vm.size,
-          'displacement': vm.displacement,
-          'fuelType': vm.fuelType,
-          'horsepower': vm.horsepower,
-          'torque': vm.torque,
-          'totalValves': vm.totalValves,
+          'configuration': vm.configuration,
         },
         'transmission':vm.transmission,
         'drivetrain':vm.drivetrain,
-        'fuelType':vm.fueltype,
+        'fuelType':vm.fuelType,
         'brakes':vm.brakes,
         'titleStatus':vm.titleStatus,
         'titleState':vm.titleState,
@@ -242,7 +237,7 @@ function VehicleAddbyVinController($scope, addbyVinProvider){
         'additionalWarranty': vm.additionalWarranty,
         'certifiedPreOwned':vm.certifiedPreOwned,
         'guaranteedFinancing': vm.guaranteedFinancing,
-        'interiorOptions' : vm.interirOptions,
+        'interiorOptions' : vm.interiorOptions,
         'exteriorOptions' : vm.exteriorOptions,
         'premiumOptions': vm.premiumOptions,
         'truckOptions': vm.truckOptions,
