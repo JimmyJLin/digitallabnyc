a599cc391bd35be033d29bfda9e70548c1ae8abb
Jimmy Lin <usmcjimmy@gmail.com>
2016-08-21 21:12:21 -0400

    added vehicle condition report - able to post to API, no vehicle conidtion view

--- app/angular-app/vehicle/vehicle_condition/addcondition.controller.js
+++ app/angular-app/vehicle/vehicle_condition/addcondition.controller.js
@@ -26,12 +26,52 @@ function AddConditionController($scope, addConditionProvider){
   var conditionData = JSON.stringify({
     'submitter': "submitter",
     'owner': '617a69c3-bd34-445c-a762-1bbcf228f7bb',
-    'condition': vm.condition,
     'vin': vm.vin,
+    'grade': vm.grade,
+    'recondition': vm.recondition,
+    'detail': vm.detail,
+    'frameDamage': vm.frameDamage,
+    'previouslyRepainted': vm.previouslyRepainted,
+    'dashLightOptions': vm.dashLightOptions,
+    'damage': vm.damage,
+    'vehicleDrive': vm.vehicleDrive,
+    'transmission': vm.transmission,
+    'exhaust': vm.exhaust,
+    'ac': vm.ac,
+    'battery': vm.battery,
+    'oilChanged': vm.oilChanged,
+    'fuelLevel': vm.fuelLevel,
+    'tireMatch': vm.tireMatch,
+    'weatherChecked': vm.weatherChecked,
+    'leftFront': vm.leftFront,
+    'rightFront': vm.rightFront,
+    'leftRear': vm.leftRear,
+    'rightRear': vm.rightRear,
+    'spare': vm.spare,
+    'jack': vm.jack,
+    'master': vm.master,
+    'remote': vm.remote,
+    'valet': vm.valet,
+    'combo': vm.combo,
+    'keylessGo': vm.keylessGo,
+    'windshield': vm.windshield,
+    'ownersManual': vm.ownersManual,
+    'odor': vm.odor,
+    'floorMats': vm.floorMats,
+    'exteriorDamageLocation': vm.exteriorDamageLocation,
+    'exteriorDamageType': vm.exteriorDamageType,
+    'exteriorDamageSeverity': vm.exteriorDamageSeverity,
+    'estimatedExteriorRepairCost': vm.estimatedExteriorRepairCost,
+    'exteriorDamageComment': vm.exteriorDamageComment,
+    'interiorDamageLocation': vm.interiorDamageLocation,
+    'interiorDamageType': vm.interiorDamageType,
+    'interiorDamageSeverity': vm.interiorDamageSeverity,
+    'estimatedInteriorRepairCost': vm.estimatedInteriorRepairCost,
+    'interiorDamageComment': vm.interiorDamageComment,
     'activity': 'add'
   })
-    console.log($scope.vin)
-    alert('clicked')
+    console.log(conditionData)
+    // alert('clicked')
     addConditionProvider.addConditionData(conditionData)
 
   }
