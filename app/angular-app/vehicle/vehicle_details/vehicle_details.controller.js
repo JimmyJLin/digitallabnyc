d3655776266cd55053cef6b9bc5969db804a2b9a
Jimmy Lin <usmcjimmy@gmail.com>
2016-08-21 20:34:32 -0400

    removed some not used selectors options and clean up not used commented out code base

--- app/angular-app/vehicle/vehicle_details/vehicle_details.controller.js
+++ app/angular-app/vehicle/vehicle_details/vehicle_details.controller.js
@@ -17,23 +17,3 @@ function VehicleDetailsController($routeParams, $scope, vehicleDetailsProvider){
       }
   })
 }
-
-// $scope.vehicle = {
-//   "ID":1,
-//   "ItemGuid":"617a69c3-bd34-445c-a762-1bbcf228f7bb",
-//   "ItemName":"",
-//   "DateCreated":"2016-08-08T21:20:34.817",
-//   "DateLastModified":"2016-08-09T07:55:04.11",
-//   "IsActive":true,
-//   "IsDeleted":false,
-//   "Notes":"",
-//   "VIN":"1GNEK13Z34J221437",
-//   "Year":"2013",
-//   "Make":"ford",
-//   "Model":"fusion",
-//   "Type":"25000",
-//   "Description":"Black, with faded sports stripes. Here are edited notes.",
-//   "Price":"25000",
-//   "OwnerGuid":"617a69c3-bd34-445c-a762-1bbcf228f7bb"
-//   }
-// }
