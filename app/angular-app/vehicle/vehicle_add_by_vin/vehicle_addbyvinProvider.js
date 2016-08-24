6a798971f8d90df796d958be125cbedf18ec5382
Jimmy Lin <usmcjimmy@gmail.com>
2016-08-23 20:50:59 -0400

    tested spinner directive with sign-in

--- app/angular-app/vehicle/vehicle_add_by_vin/vehicle_addbyvinProvider.js
+++ app/angular-app/vehicle/vehicle_add_by_vin/vehicle_addbyvinProvider.js
@@ -28,9 +28,12 @@
 
       this.addVehicle = function(addVehicleData){
         console.log(addVehicleData)
+        $scope.loading=true;
+
         $http.post('http://api.nationsauction.com/inventory/Vehicle/Add', addVehicleData)
           .then(function(response){
             console.log('this is the response', response)
+            $scope.loading=false;
           })
           .catch(function(error){
             console.log("Unable to Add vehicle, error: ", error)
