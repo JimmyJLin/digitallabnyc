4435a77e1bfb83081171cd61214337baa6567ba3
Brian Fling <fling@me.com>
2016-08-25 23:33:29 -0700

    loader styling

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -185,7 +185,7 @@ naBaseApp.directive('loading', function () {
   return {
     restrict: 'E',
     replace: true,
-    template: '<div class="loading"><img src="/assets/images/spinner/spinner.gif" width="40" height="40"/>LOADING...</div>',
+    template: '<div class="loading"><div class="ajax-spinner-bars"><div class="bar-1"></div><div class="bar-2"></div><div class="bar-3"></div><div class="bar-4"></div><div class="bar-5"></div><div class="bar-6"></div><div class="bar-7"></div><div class="bar-8"></div><div class="bar-9"></div><div class="bar-10"></div><div class="bar-11"></div><div class="bar-12"></div><div class="bar-13"></div><div class="bar-14"></div><div class="bar-15"></div><div class="bar-16"></div></div></div>',
     link: function (scope, element, attr) {
       scope.$watch('loading', function (val) {
           if (val)
